32745128a7bbfce0c2e8bd50e29f7e04b7a138f8
Taylor Kipp <taylorofkipp@gmail.com>
2026-03-14 14:36:09 -0400

    Remove committed codex smoke artifacts

--- _codex_smoke/out_blackhole_v2/native-bundle-smoke-native-ui/src/main.rs
+++
@@ -1,7 +0,0 @@
-use kain_ui_native::run_bundled_app_json;
-
-const KAIN_RUNTIME_BUNDLE: &str = include_str!("../generated/native_app_bundle.json");
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run_bundled_app_json(KAIN_RUNTIME_BUNDLE)
-}
