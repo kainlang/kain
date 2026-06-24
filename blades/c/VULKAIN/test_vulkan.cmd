06f0964c846a770bd6b3237e44b94183ca867a2b
Taylor Kipp <taylorofkipp@gmail.com>
2026-06-24 03:57:18 -0400

    Remove VULKAIN example files
    Delete the VULKAIN example/bridge/build scripts and sources (multiple files under blades/c/VULKAIN and blades/c/vulkain/KAIN.toml) to remove the Vulkan demo/loader/bridge artifacts. Also simplify blades/window_proof/src/component_composition.kn by trimming the large header/comment block (retains the runtime import and file).

--- blades/c/VULKAIN/test_vulkan.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 > nul
-clang X:\blades\c\VULKAIN\test_vulkan.c C:\VulkanSDK\1.4.350.0\Lib\vulkan-1.lib -IC:\VulkanSDK\1.4.350.0\Include -o X:\blades\c\VULKAIN\test_vulkan.exe
-echo CLANG EXIT: %ERRORLEVEL%
-X:\blades\c\VULKAIN\test_vulkan.exe
-echo EXE EXIT: %ERRORLEVEL%
