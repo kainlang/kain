5e55d961cbf7e7d7eebbd307a89a7b6608eebc33
Taylor Kipp <taylorofkipp@gmail.com>
2026-06-26 20:50:35 -0400

    Refactor python directory into amalgamated capsules with --raw for cleaner layout and directory

--- blades/python/kainbleton/src/native/kainbleton_bridge.h
+++
@@ -1,7 +0,0 @@
-#ifndef KAINBLETON_BRIDGE_H
-#define KAINBLETON_BRIDGE_H
-
-int kainbleton_bridge_signature(int frames, int tracks, int clips, int salt);
-int kainbleton_bridge_meter_color(int track, int frame, int seed);
-
-#endif
